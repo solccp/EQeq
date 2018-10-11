73cfe07bb53f59df7c9edce14b00ef769739d838
Daniele Ongari <daniele.ongari@gmail.com>
2018-10-11 10:52:58 +0200

    added options to example

--- examples/HKUST1/run.sh
+++ examples/HKUST1/run.sh
@@ -2,7 +2,16 @@
 
 rm -rf output
 
-../../eqeq HKUST1.cif &> EQeq.log
+lambda=1.2          #(default: 1.2) The dielectric screening parameter
+hI0=-2.0            #(default: -2.0) The electron affinity of hydrogen
+chargePrecision=3   #(default: 3) Number of digits to use for point charge
+method="ewald"      #(default: "ewald", alternative: "nonperiodic") Method to compute the Coulombic interaction
+mR=2                #(default: 2) Number of unit cells to consider in per. calc. ("real space")
+mK=2                #(default: 2) Number of unit cells to consider in per. calc. ("frequency space")
+eta=50              #(default: 50) Ewald splitting parameter
+
+ 
+../../eqeq HKUST1.cif $lambda $hI0 $chargePrecision $method $mR $mK $eta &> EQeq.log
 
 mkdir output 
 mv *EQeq* output
